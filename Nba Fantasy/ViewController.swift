d2ba496c73765e0aae956fb53dda2ee434970a52
Vinicius <viniciuscorreia11@gmail.com>
2017-10-25 12:54:10 -0300

    Initial Commit

---
+++ Nba Fantasy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nba Fantasy
+//
+//  Created by Vinicius Mangueira Correia on 25/10/17.
+//  Copyright © 2017 Vinicius Mangueira Correia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
